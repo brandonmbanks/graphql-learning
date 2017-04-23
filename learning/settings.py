6f941c5dc6031d680fbe3e0d6b742726c65d159a
Brandon Banks <b93banks@gmail.com>
2017-04-23 18:27:37 -0400

    add admin user

--- learning/settings.py
+++ learning/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '_pde*0#-4jmkn-srn^yqmtru#!+)jm3@_0td9rn-0_6-030^_c'
 
+ADMIN_PASSWORD = 'W#!tLGN5ZmxAiM2LQV3!'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
