1056047f12afea3df5115b834ebb39328d69ca0d
Brandon Banks <b93banks@gmail.com>
2017-04-23 19:09:27 -0400

    change table names for artists and songs

--- store/models/artist.py
+++ store/models/artist.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Artist(models.Model):
+
+    class Meta:
+        db_table = 'artists'
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=200)
--- store/models/song.py
+++ store/models/song.py
@@ -3,6 +3,10 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Song(models.Model):
+
+    class Meta:
+        db_table = 'songs'
+
     title = models.CharField(max_length=100)
     artist_id = models.CharField(max_length=50)
     release_date = models.DateField()
