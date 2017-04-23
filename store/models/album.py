65e482bc6ed75fd4a46feea9773e2974c0108ca2
Brandon Banks <b93banks@gmail.com>
2017-04-23 19:51:14 -0400

    add album and foreign key

--- store/models/album.py
+++ store/models/album.py
@@ -2,14 +2,13 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-class Song(models.Model):
+class Album(models.Model):
 
     class Meta:
-        db_table = 'songs'
+        db_table = 'albums'
 
     title = models.CharField(max_length=100)
     artist_id = models.CharField(max_length=50)
-    album_id = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
